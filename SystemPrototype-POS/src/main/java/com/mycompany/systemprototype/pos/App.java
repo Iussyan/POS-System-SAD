a947af6b173727a507831b9a3681634a8848ad82
QCU <QCU@DESKTOP-NFJ8DMQ>
2024-11-18 21:15:39 +0800

    test

--- SystemPrototype-POS/src/main/java/com/mycompany/systemprototype/pos/App.java
+++ SystemPrototype-POS/src/main/java/com/mycompany/systemprototype/pos/App.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-    // NICELY DONE YOU DUMB FUCK
+    // NICELY DONE YOU DUMB FUCKING BITCH
 
     @Override
     public void start(Stage stage) throws IOException {
